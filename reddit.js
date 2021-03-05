e9c8b90e930c8150b05a74cb63a39efaeaf2090c
H-Thomas4 <77423516+H-Thomas4@users.noreply.github.com>
2021-03-05 03:33:40 -0600

    Add files via upload

---
+++ reddit.js
@@ -0,0 +1,28 @@
+const path = require ("path");
+const fs = require ("fs");
+const rp = require ('request-promise');
+const { post } = require("request-promise");
+
+rp("https://reddit.com/r/popular.json")
+    .then(html => {
+
+        let posts = JSON.parse(html).data.children;
+        let objs = [];
+
+        post.foreach((item) => {
+            let obj = {
+                title: item.data.title,
+                url: item.data.url,
+                author: item.data.author
+            }
+            objs.push(obj);
+        });
+
+        fs.writeFile('popular-articles.json', JSON.stringify(objs), (err) => {
+            if (err) console.log(err);
+        });
+
+    })
+    .catch(err => {
+        
+    })
