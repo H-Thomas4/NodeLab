14b81db6849f521ef38117561832cd9bd0109e8e
H-Thomas4 <77423516+H-Thomas4@users.noreply.github.com>
2021-03-05 03:31:36 -0600

    Delete server.js

--- Server/server.js
+++
@@ -1,9 +0,0 @@
-var path = require ('path');
-var fs = require ('fs');
-
-fs.readFile(path.join(_dirname, 'chirps.json'), function(err,data) {
-    if(err) return console.log(err);
-
-    console.log(data.toString());
-});
-
